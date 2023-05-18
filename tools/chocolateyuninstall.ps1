3d6cf440f623da94383ee65f321acf72a1f6856c
Ben Lau <benjamin.a.lau@gmail.com>
2023-05-18 14:26:20 -0700

    refactor to remove errors shown in editor

--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
 [array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
 
 if ($key.Count -eq 1) {
-  $key | % {
+  $key | ForEach-Object {
     $packageArgs['file'] = "$($_.UninstallString)"
 
     if ($packageArgs['fileType'] -eq 'MSI') {
@@ -27,5 +27,5 @@ if ($key.Count -eq 1) {
   Write-Warning "$($key.Count) matches found!"
   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
   Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $($_.DisplayName)"}
+  $key | ForEach-Object {Write-Warning "- $($_.DisplayName)"}
 }
